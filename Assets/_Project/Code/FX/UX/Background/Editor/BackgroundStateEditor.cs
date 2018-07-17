b8ae44a07e6636598bf0e9b0b8208f854fc09e1e
Keenan Woodall <keenanwoodall@gmail.com>
2018-07-17 02:48:34 -0500

    Added previewing to background state editing.

---
+++ Assets/_Project/Code/FX/UX/Background/Editor/BackgroundStateEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Sycamore.FX.UX
+{
+	[CustomEditor (typeof (BackgroundState))]
+	public class BackgroundStateEditor : Editor
+	{
+		private BackgroundManager managerInstance;
+
+		public override void OnInspectorGUI ()
+		{
+			base.OnInspectorGUI ();
+
+			if (managerInstance == null)
+				managerInstance = FindObjectOfType<BackgroundManager> ();
+			if (managerInstance == null)
+				return;
+
+			var state = target as BackgroundState;
+
+			managerInstance.SetColorA (state.colorA);
+			managerInstance.SetColorB (state.colorB);
+			managerInstance.SetScale (state.scale);
+			managerInstance.SetOctaves (state.octaves);
+			managerInstance.SetSpeed (state.speed);
+			managerInstance.SetBias (state.bias);
+		}
+	}
+}
